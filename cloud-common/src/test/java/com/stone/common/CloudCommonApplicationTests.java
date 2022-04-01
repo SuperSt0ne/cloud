f7f27bd8de6b5e2b7e1fb0ab7ed44d0b769d562e
兰戈 <2380584338@qq.com>
2022-04-01 11:12:58 +0800

    add: common模块及依赖

---
+++ cloud-common/src/test/java/com/stone/common/CloudCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stone.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
