c5168f914c341d2bed4159cea73f29be193bfe64
兰戈 <2380584338@qq.com>
2022-05-23 22:29:27 +0800

    add: ResultCode description

--- cloud-common/src/main/java/com/stone/common/result/ResultCode.java
+++ cloud-common/src/main/java/com/stone/common/result/ResultCode.java
@@ -2,9 +2,18 @@ package com.stone.common.result;
 
 public class ResultCode {
 
+    /**
+     * 请求成功
+     */
     public static final String CODE_SUCCESS = "100001";
 
+    /**
+     * 全局(主动)异常
+     */
     public static final String CODE_GLOBAL_EXCEPTION = "500001";
 
+    /**
+     * 未知异常
+     */
     public static final String CODE_UNEXPECTED_EXCEPTION = "500002";
 }
