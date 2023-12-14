09826b8cbbdb1ed95cf53582f1165d7e539a82bd
兰戈 <2380584338@qq.com>
2023-12-14 20:41:17 +0800

    TransFlatMapApi 描述

--- cloud-flink/src/main/java/com/stone/flink/api/base/trans/TransFlatMapApi.java
+++ cloud-flink/src/main/java/com/stone/flink/api/base/trans/TransFlatMapApi.java
@@ -1,12 +1,17 @@
 package com.stone.flink.api.base.trans;
 
 import com.stone.sdk.flink.bean.Event;
+import com.stone.sdk.flink.source.CustomUserOptSource;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
+/**
+ * flatMap 是 Flink 中的一个转换操作，用于将一个数据集的元素拆分成多个元素，并将它们输出到新的数据集中。
+ * 它与 map 操作相似，但 map 操作将每个元素转换为一个新的元素，而 flatMap 操作将每个元素转换为零个或多个元素。
+ */
 public class TransFlatMapApi {
 
     public static void main(String[] args) throws Exception {
